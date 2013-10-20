35808e36e88e19a18c3a8bd32441ebb6bb291023
Daniel Libanori <daniellibanori@gmail.com>
2013-10-20 18:41:54 -0200

    Use HAML instead ERB

--- app/views/votes/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @vote, :word_id, :coloer_id, :created_at, :updated_at
---
+++ config/initializers/haml.rb
@@ -0,0 +1,5 @@
+Alpha::Application.configure do
+  config.generators do |g|
+    g.template_engine :haml
+  end
+end
